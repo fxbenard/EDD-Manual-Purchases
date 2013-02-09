d4f0f16c50e68d2405e2c6981d15d68bb5582e31
Pippin Williamson <pippin@pippinsplugins.com>
2013-02-09 15:24:07 -0600

    Update cap check

--- edd-manual-purchases.php
+++ edd-manual-purchases.php
@@ -131,7 +131,7 @@ class EDD_Manual_Purchases {
 
 	public static function submenu() {
 		global $edd_create_payment_page;
-		$edd_create_payment_page = add_submenu_page( 'options.php', __('Create Payment', 'edd-manual-purchases'), __('Create Payment', 'edd-manual-purchases'), 'manage_options', 'edd-manual-purchase', array( __CLASS__, 'payment_creation_form' ) );
+		$edd_create_payment_page = add_submenu_page( 'options.php', __('Create Payment', 'edd-manual-purchases'), __('Create Payment', 'edd-manual-purchases'), 'view_shop_reports', 'edd-manual-purchase', array( __CLASS__, 'payment_creation_form' ) );
 	}
 
 	public static function payment_creation_form() {
